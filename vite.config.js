e124cdd10ff7005e14dbb9491cdce6c42a498c8b
Yerassyl <theycallmeinvo@gmail.com>
2025-01-14 01:24:58 +0500

    fix reponame

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/REPONAME/',
+  base: '/senim/',
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
